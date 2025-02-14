3fcb440122b0c62553d8e1cc091e50a06f16c78f
unknown <victoria.borges027@gmail.com>
2025-02-14 15:21:22 -0300

    Atualização: adicionando novas classes e modificando Menu

--- conta/Menu.java
+++ conta/Menu.java
@@ -2,19 +2,42 @@ package conta;
 
 import java.util.Scanner;
 import conta.util.Cores;
-import conta.model.Conta;
+import conta.model.ContaCorrente;
+import conta.model.ContaPoupanca;
 
 public class Menu {
 
 	public static void main(String[] args) {
 	
 	
-		Conta c1 = new Conta(1, 123, 1, "Victoria", 10000.0f);
-		c1.visualizar();
-		c1.sacar(12000.0f);
-		c1.visualizar();
-		c1.depositar(5000.0f);
-		c1.visualizar();
+		// teste da classe conta corrente
+		System.out.println(Cores.TEXT_CYAN_BOLD);
+		ContaCorrente cc1 = new ContaCorrente(2,123,1,"Mariana",15000.0f,1000.0f);
+		cc1.visualizar();
+		cc1.sacar(12000.0f);
+		cc1.visualizar();
+		cc1.depositar(5000.0f);
+		cc1.visualizar();
+		
+		
+		// teste da classe conta poupança
+		System.out.println(Cores.TEXT_GREEN);
+		ContaPoupanca cp1 = new ContaPoupanca(3,123,2,"Victor",100000.f,15);
+		cp1.visualizar();
+		cp1.sacar(1000.0f);
+		cp1.visualizar();
+		cp1.depositar(5000.0f);
+		cp1.visualizar();
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		
 		Scanner leia = new Scanner(System.in);
 		int opcao;
